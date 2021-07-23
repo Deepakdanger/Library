ce05a82a19f29e1464baa3724c24fe8db1116aa8
Deepakdanger <depk.96.kumar@gmail.com>
2021-07-23 16:17:14 +0530

    form for book

---
+++ lib.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+let books=[];
+inputElement = document.getElementById('#book').value;
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  const book = 
+  books.push(book)
+}
