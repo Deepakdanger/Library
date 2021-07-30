ee44efe99a3ecc6b03d0633aaf57faa22a673e86
Deepakdanger <depk.96.kumar@gmail.com>
2021-07-30 21:56:03 +0530

    Submit the book

--- lib.js
+++ lib.js
@@ -1,12 +1,35 @@
 let myLibrary = [];
-let books=[];
-inputElement = document.getElementById('#book').value;
-function Book() {
-  // the constructor...
+//inputElement = document.getElementById('#book').value;
+const books = document.getElementById('book_list');
+const form = document.getElementById('forms');
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
-  const book = 
-  books.push(book)
+function addBookToLibrary(name, author, pages, read) {
+    const newb = new Book(name, author, pages, read);
+    myLibrary.push(newb);
 }
+
+function submit(form, event, readCheck) {
+    event.preventDefault();
+    const bookInfo = Array.from(form.elements).map((ele) => ele.value);
+    const readStatus = Boolean(readCheck.checked);
+    submitBook(bookInfo, readStatus);
+    form.reset();
+}
+
+function submitBook(bookInfo, readStatus) {
+    const [name, author, pages] = bookInfo;
+    addBookToLibrary(name, author, pages, readStatus);
+}
+
+const list = document.createElement('ul');
+books.appendChild(list);
+
+form.addEventListener('submit', (event) => submit(form, event, readCheck));
+
