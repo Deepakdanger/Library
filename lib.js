553cc84069077540d69f4eb8ea2199e04d9ef0ec
Deepakdanger <depk.96.kumar@gmail.com>
2021-08-03 19:23:49 +0530

    Display books

--- lib.js
+++ lib.js
@@ -2,6 +2,9 @@ let myLibrary = [];
 //inputElement = document.getElementById('#book').value;
 const books = document.getElementById('book_list');
 const form = document.getElementById('forms');
+form.addEventListener('submit', (e) => submitFunction(form, e));
+const newBook = document.getElementById('addBook');
+newBook.addEventListener('click', () => classToggle(form));
 
 function Book(name, author, pages, read) {
     this.name = name;
@@ -11,25 +14,32 @@ function Book(name, author, pages, read) {
 }
 
 function addBookToLibrary(name, author, pages, read) {
-    const newb = new Book(name, author, pages, read);
-    myLibrary.push(newb);
+    const newBook = new Book(name, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    displayLibrary(myLibrary)
 }
 
-function submit(form, event, readCheck) {
-    event.preventDefault();
+function submitFunction(form, e) {
+    e.preventDefault();
     const bookInfo = Array.from(form.elements).map((ele) => ele.value);
-    const readStatus = Boolean(readCheck.checked);
-    submitBook(bookInfo, readStatus);
+    const bookRead = Boolean(read1.checked);
+    const [name, author, pages] = bookInfo;
+    addBookToLibrary(name, author, pages, bookRead);
     form.reset();
 }
 
-function submitBook(bookInfo, readStatus) {
-    const [name, author, pages] = bookInfo;
-    addBookToLibrary(name, author, pages, readStatus);
+function displayLibrary(myLibrary) {  
+    const para = document.createElement("div");
+    const currentDiv  = document.getElementById("book_list");
+    let currentTable =myLibrary[myLibrary.length-1];
+    para.innerHTML= `${currentTable.name} ${currentTable.author}
+    ${currentTable.pages}
+    ${currentTable.read}    
+    `;
+    document.body.insertBefore(para, currentDiv);
+} 
+
+function classToggle(form) {
+    form.style.visibility = 'visible';
 }
-
-const list = document.createElement('ul');
-books.appendChild(list);
-
-form.addEventListener('submit', (event) => submit(form, event, readCheck));
-
