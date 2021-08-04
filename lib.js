27fb66a4ac75b2a61893f6461eebb4e9bf1bbf47
Deepakdanger <depk.96.kumar@gmail.com>
2021-08-04 13:55:08 +0530

    Add linters

--- lib.js
+++ lib.js
@@ -1,70 +1,64 @@
-let myLibrary = [];
-//inputElement = document.getElementById('#book').value;
+/* eslint-disable no-use-before-define */
+const myLibrary = [];
 const books = document.getElementById('book_list');
 
 const form = document.getElementById('forms');
-form.addEventListener('submit', (e) => submitFunction(form, e));
+const read1 = document.getElementById('read1');
 
 const newBook = document.getElementById('addBook');
-newBook.addEventListener('click', () => classToggle(form));
-
-const readToggle = document.getElementById('readen');
 
 function Book(name, author, pages, read) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBookToLibrary(name, author, pages, read) {
-    const newBook = new Book(name, author, pages, read);
-    myLibrary.push(newBook);
-    console.log(myLibrary);
-    displayLibrary(myLibrary)
-}
-
-function submitFunction(form, e) {
-    e.preventDefault();
-    const bookInfo = Array.from(form.elements).map((ele) => ele.value);
-    const bookRead = Boolean(read1.checked);
-    const [name, author, pages] = bookInfo;
-    addBookToLibrary(name, author, pages, bookRead);
-    form.reset();
-}
-
-function displayLibrary(myLibrary) {  
-    books.innerHTML = '';
-    for (let i = 0; i < myLibrary.length; i++) {
-        const para = document.createElement("div");
-        // const deleteBook = document.createElement("BUTTON");
-        // deleteBook.innerHTML = "Delete";
-        //deleteBook.onclick = deletethisBook(i,myLibrary);
-        const currentDiv  = document.getElementById("book_list");
-        let currentTable = myLibrary[i];
-        para.innerHTML= `${currentTable.name} ${currentTable.author}
-        ${currentTable.pages}
-        ${currentTable.read} <button onclick = deletethisBook(${i},myLibrary)> Delete </button>
-        <button onclick = changeStatus(${i},myLibrary)> Change Status</button>
-        `;
-        // para.appendChild(deleteBook); 
-        // const delete1 = para.lastChild;
-        // console.log(delete1);
-        currentDiv.appendChild(para);
-    }
-    
-} 
-
-function classToggle(form) {
-    form.style.visibility = 'visible';
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function deletethisBook(i, myLibrary) {
-    myLibrary.splice(i,1);
-    displayLibrary(myLibrary);
+  myLibrary.splice(i, 1);
+  displayLibrary(myLibrary);
 }
 
 function changeStatus(i, myLibrary) {
-    myLibrary[i].read = !myLibrary[i].read;
-    displayLibrary(myLibrary);
+  myLibrary[i].read = !myLibrary[i].read;
+  displayLibrary(myLibrary);
 }
+
+function displayLibrary(myLibrary) {
+  books.innerHTML = '';
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    const para = document.createElement('div');
+    const currentDiv = document.getElementById('book_list');
+    const currentTable = myLibrary[i];
+    para.innerHTML = `${currentTable.name} ${currentTable.author}
+          ${currentTable.pages}
+          ${currentTable.read} <button onclick = ${deletethisBook(i, myLibrary)}> Delete </button>
+          <button onclick = ${changeStatus(i, myLibrary)}> Change Status</button>
+          `;
+    currentDiv.appendChild(para);
+  }
+}
+
+function addBookToLibrary(name, author, pages, read) {
+  const newBook = new Book(name, author, pages, read);
+  myLibrary.push(newBook);
+  displayLibrary(myLibrary);
+}
+
+function classToggle(form) {
+  form.style.visibility = 'visible';
+}
+
+function submitFunction(form, e, read1) {
+  e.preventDefault();
+  const bookInfo = Array.from(form.elements).map((ele) => ele.value);
+  const bookRead = Boolean(read1.checked);
+  const [name, author, pages] = bookInfo;
+  addBookToLibrary(name, author, pages, bookRead);
+  form.reset();
+  classToggle(form);
+}
+
+form.addEventListener('submit', (e) => submitFunction(form, e, read1));
+
+newBook.addEventListener('click', () => classToggle(form));
